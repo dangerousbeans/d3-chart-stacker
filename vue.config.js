2f8d35afeedbbd9c84ee04738314db03e3830b87
Joran Kikke <joran.k@gmail.com>
2019-11-24 11:19:45 +1300

    github pages deploy setup

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    runtimeCompiler: true,
+    lintOnSave: false,
+    
+    // Github pages setup
+    publicPath: 
+      process.env.NODE_ENV === 'production'
+        ? '/d3-chart-stacker/'
+        : '/'
+    
+  
+  }
