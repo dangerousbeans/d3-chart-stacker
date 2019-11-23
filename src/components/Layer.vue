4bee009ace23158775620c84a7cf2855e8dfe94a
Joran Kikke <joran.k@gmail.com>
2019-11-24 11:08:55 +1300

    draggable list of layers

--- src/components/Layer.vue
+++ src/components/Layer.vue
@@ -1,12 +1,16 @@
 <template>
-  <div >
-  </div>
+  <b-list-group-item >
+    {{ layer.name }}
+  </b-list-group-item>
 </template>
 
 <script>
 export default {
   name: 'Layer',
   props: {
+    layer: {
+      type: Object
+    }
   }
 }
 </script>
--- src/components/Layers.vue
+++ src/components/Layers.vue
@@ -1,13 +1,40 @@
 <template>
   <div >
-    Layers
+    <h3>Layers</h3>
+
+    <b-list-group>
+      <draggable v-model="layers" group="layers" @start="drag=true" @end="drag=false">
+        <Layer v-for="layer in layers" v-bind:key="layer.key" :layer="layer"></Layer>
+      </draggable>
+    </b-list-group>
   </div>
 </template>
 
 <script>
+import Layer from './Layer.vue'
+import draggable from 'vuedraggable'
+
 export default {
   name: 'Layers',
-  props: {
+  components: {
+    Layer,
+    draggable
+  },
+  data() {
+    return {
+      layers: [
+        {
+          key: 1,
+          name: "energy use",
+          values: [ 1,2,3 ]
+        },
+        {
+          key: 2,
+          name: "energy consumption",
+          values: [ 1,2,3 ]
+        }
+      ]
+    }
   }
 }
 </script>
